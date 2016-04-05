4083a0c285a1dd31cfe320fb4cec060e443c3eb6
Sekhar Paladugu <sekharp@gmail.com>
2016-04-05 13:40:12 -0600

    new implementation passed all tests except last two, did argument error raise for diff. string length

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,11 +3,15 @@ require 'pry'
 class Hamming
   def self.compute(first, second)
     distance = 0
-    first.chars.each_with_index do |char, index|
-      if char != second[index]
-        distance += 1
+    if first.length == second.length
+      first.chars.each_with_index do |char, index|
+        if char != second[index]
+          distance += 1
+        end
       end
+      return distance
+    else
+      raise ArgumentError, 'Strands must be the same length!'
     end
-    return distance
   end
 end
