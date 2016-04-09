a1bd8d653f9fcc0c127a024b4a64a7b54695a7c8
Sekhar Paladugu <sekharp@gmail.com>
2016-04-09 10:46:26 -0500

    first test past rna transcription

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,4 +1,19 @@
+require 'pry'
+
 class Complement
-  def self.of_dna(input)
+  def self.of_dna(rna)
+    rna = rna.chars
+    rna.map! do |nuc|
+      if nuc == 'C'
+        nuc = 'G'
+      elsif nuc == 'G'
+        nuc = 'C'
+      elsif nuc == 'T'
+        nuc = 'A'
+      else
+        nuc = 'U'
+      end
+    end
+    rna.join
   end
 end
