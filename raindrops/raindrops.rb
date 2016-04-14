bde4f93c6dead0245fa815e46c3c38d87dbe12e8
Sekhar Paladugu <sekharp@gmail.com>
2016-04-14 10:41:56 -0600

    fourth test passing

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -4,6 +4,10 @@ class Raindrops
   def self.convert(drops)
     if drops % 3 == 0
       'Pling'
+    elsif drops % 5 == 0
+      'Plang'
+    elsif drops % 7 == 0
+      'Plong'
     else
       "#{drops}"
     end
